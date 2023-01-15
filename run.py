0fe4220ee81e8230d5e7a424da3d1de364546255
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2023-01-15 13:32:53 +0000

    imported gspread module, created credential scopes and connected the user-database-spreadsheet to the programm

--- run.py
+++ run.py
@@ -1,8 +1,23 @@
+import gspread 
+from google.oauth2.service_account import Credentials
+
 import curses
 import os
 from curses import wrapper
 from curses.textpad import Textbox, rectangle
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('project3-users')
+
+users = SHEET.worksheet('users-database') 
+print(users)
 
 print('This is a typing game to enhance your programming typing skills:')
 #user_name = input('What is your username?\n')
