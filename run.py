a0df3550a842f3493da5a8cf18942a95196d20ab
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2023-01-15 12:18:02 +0000

    debugging code indentation with heroku

--- run.py
+++ run.py
@@ -1,28 +1,27 @@
 import curses
 from curses import wrapper
 from curses.textpad import Textbox, rectangle
-import time
-import select
+
+
 print('This is a typing game to enhance your programming typing skills:')
 user_name = input('What is your username?\n')
 
 
 def main(stdscr):
-     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_YELLOW)
-     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-     BLUE_AND_YELLOW = curses.color_pair(1)
-     GREEN_AND_BLACK = curses.color_pair(2)
+    #  curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_YELLOW)
+    #  curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    #  BLUE_AND_YELLOW = curses.color_pair(1)
+    #  GREEN_AND_BLACK = curses.color_pair(2)
 
-     win = curses.newwin(8, 100 , 2, 2)
-     
-     box = Textbox(win)
-     rectangle(stdscr, 2, 2, 8, 100)
-     stdscr.refresh()
-     box.edit()
-     text = box.gather().strip().replace("\n", "")
-     stdscr.addstr(5, 20, text)
-     stdscr.addstr('message')
-     stdscr.getch()
+    win = curses.newwin(8, 100, 2, 2)
+    box = Textbox(win)
+    rectangle(stdscr, 2, 2, 8, 100)
+    stdscr.refresh()
+    box.edit()
+    text = box.gather().strip().replace("\n", "")
+    stdscr.addstr(5, 20, text)
+    stdscr.addstr('message')
+    stdscr.getch()
      
 
 wrapper(main)
