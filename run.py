3670b8afa137a0e5cb590f24e3ef6477cbbd90a7
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2023-01-17 19:41:28 +0000

    finished auth functionality, added game function to implement the gaming environment

--- run.py
+++ run.py
@@ -19,7 +19,9 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('project3-users')
-ERROR = ''
+ERROR = ' '
+GEEN_MESSAGE = ' '
+LOGGED_IN = False
 
 def is_not_existing_user(input_username):
     """
@@ -35,26 +37,22 @@ def is_not_existing_user(input_username):
     return is_new_user
     
 
-def create_password():
+def create_password(salt):
     """
     Will return a hashed password 
     """
-    global ERROR
-    
     password = ''
     while True:
         x = getch.getch()
         if x == '\r' or x == '\n':
             break
         print('*', end='', flush=True)
-        password +=x
-        
+        password += x
+            
     encoded_psw = password.encode('utf-8')
-    salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(encoded_psw, salt)
-    print(hashed)
+    print('salt: ',salt)
     return hashed
-    print("\nout=", getPass()) 
 
     
 def create_account():
@@ -62,23 +60,62 @@ def create_account():
     Gets called when creating a new account
     """
     global ERROR
+    global GREEN_MESSAGE
     username_input = input('create a unique username:\n')
     
     if is_not_existing_user(username_input) == True:
-        print('great')
-        credentials = [username_input, f'{create_password()}']
+        print('Create a password')
+        salt = bcrypt.gensalt()
+        credentials = [username_input, f'{create_password(salt)}', f"{salt}"]
         users = SHEET.worksheet('users-database').append_row(credentials)
+        GEEN_MESSAGE = colored('succesfully registered', 'green', attrs=['reverse', 'blink'])
+        print(GREEN_MESSAGE)
+        return True
         
     else:
         ERROR = colored('this Username allready exists', 'red', attrs=['reverse', 'blink'])
-        
+        os.system('clear')
+        print(ERROR)
+        create_account() #red-case recursive call to the same function. 
 
+        
+def login_check_password(db_user_password, user_salt):
+    """
+    Gets the user password and salt from the database and compares it to the user input for validation 
+    """
+    user_input_password =  f'{create_password(user_salt)}'  # bring the salt into the game for pasword matching
+    if user_input_password != db_user_password:
+        ERROR = colored('wrong password', 'red', attrs=['reverse', 'blink'])
+        print(ERROR)
+        login_check_password(db_user_password, user_salt) #red-case recursive call to the same function.
+    return True
+
+    
 def login():
     """
     Gets called when logging into an existing account
     """   
+    global GREEN_MESSAGE
     username_input = input('feed your username to me:\n')
-    
+    if is_not_existing_user(username_input) == False:
+        users = SHEET.worksheet('users-database')
+        usernames = users.col_values(1)
+        passwords = users.col_values(2)
+        salts = users.col_values(3)
+        user_credentials = [[username, password, salt] for username, password, salt in zip(usernames,passwords, salts) if username_input == username][0]  #make a zip method and retrieve credentials
+        print(user_credentials)
+        print('type your password:')
+        encoded_salt = user_credentials[2][2:-1].encode('utf-8')          
+        login_check_password(user_credentials[1], encoded_salt)
+        print('great Stuff you are now logged in') 
+        GREEN_MESSAGE = colored('Successfuly logged in', 'green', attrs=['reverse', 'blink'])   
+        print(GREEN_MESSAGE)
+        return True
+    else:
+        ERROR = colored('this Username does not exist', 'red', attrs=['reverse', 'blink'])
+        os.system('clear')
+        print(ERROR)
+        login() #red-case recursive call to the same function. 
 
         
 def auth():
@@ -87,34 +124,45 @@ def auth():
     with the appropriate exceptions and permissions
     """
     global ERROR
-    
+    global GEEN_MESSAGE
+    global LOGGED_IN 
+    print(ERROR)
     print('1. Login')
     print('2. Register')
     user_auth_type = input('type one of the command numbers above in order to proceed \n')
     if user_auth_type == '2':
-        create_account()
+         create_account()            
     elif user_auth_type == '1':
-        login()
+        if login() == True:
+            LOGGED_IN = True
     else:
         ERROR = colored('please type in one of the two digit-options in the menu', 'red', attrs=['reverse', 'blink'])
         
         
-
+def game():
+    """
+    will hold the typing game curses code
+    """
+    print("let's begin the fun")
+    
 def main():
     """
     Builds the app environment and calls all the functions and messages
     """
-    global ERROR
-    print(ERROR)
-    print('This is a typing game to enhance your programming typing skills:')
-   
-    auth()
-    
-    
-while True:
-    #os.system('clear') 
-    print('\n\n\n')
-    main()
+    global LOGGED_IN
+    if LOGGED_IN == False:
+        print(ERROR)
+        print('This is a typing game to enhance your programming typing skills:')
+        auth()
+    if LOGGED_IN == True:
+        os.system('clear')
+        print(GREEN_MESSAGE)
+        print('This is a typing game to enhance your programming typing skills:')
+        game()
+        
+          
+print('\n\n\n')
+main()
    
 
 
@@ -129,3 +177,9 @@ while True:
 # text = colored('Hello, World!', 'yellow', attrs=['reverse', 'blink'])
 # print(text)
 # print(Fore.RED + 'This text is red in color')
+
+# pygraph witht he dependencies / 3rd party loibraries
+# technical design - flow 
+# user manual - bullte points
+# code Institute python validator.
+# Unitesting 
